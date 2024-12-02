08652bc3f79748635f1f64616e97c1641039c194
Abdallah Al Sawae´er <ajarmah@Abdallahs-Air.fritz.box>
2024-12-02 18:21:56 +0100

    Map through food data and pass it as props to the Card component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
+import data from "./data"
 import './App.css'
+import Card from "./Card"
 
 function App() {
+  const cards = data.map(card => {
+    console.log(data)
+    return(
+      <Card
+        key={card.id}
+        {...card}
+      />
+    )
+  })
 
   return (
     <>
+    {cards}
     </>
   )
 }
---
+++ src/Card.jsx
@@ -0,0 +1,10 @@
+function Card(props){
+    return(
+        <>
+            Title: { props.title} <br></br>
+            Description: { props.text} <br></br>
+        </>
+    )
+}
+
+export default Card
