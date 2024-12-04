0fa8e7ce7e97ad0169cf896823097dd62d897701
Abdallah Al Sawae´er <ajarmah@Abdallahs-Air.fritz.box>
2024-12-04 12:14:34 +0100

    add toggle functionality for star state (preparation for favorites)

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react"
+import FilledStar from "../public/images/filled-star.png"
+import EmptyStar from "../public/images/star.png"
+
+
 function Card(props){
+    const[isFilled, setIsFilled] = useState(true);
+    function toggle(){
+        setIsFilled(!isFilled)
+    };
+
+    const status = isFilled ? FilledStar : EmptyStar;
+
     return(
         <>
+          
             Title: { props.title} <br></br>
             Description: { props.text} <br></br>
+            <img onClick={toggle} src={status} width='50px' height='50px'/>
+            
         </>
     )
 }
